e326fc9b72e4f29ac66801b8dd74ea21e763e2d9
Nitin Sharma <95009807+ignitinsharma@users.noreply.github.com>
2023-01-12 18:41:11 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,16 @@ import { useContext } from "react";
 import { AppContext } from "./ContextAPI/AppContextProvider";
 
 function App() {
+
+  /* 
+  --> For access that AppContext data we have to use useContext Function and inside 
+      this name of which context we created --> useContext(AppContext)
+
+      Then we simple destructuring that this we want to use and then simpliy
+      using we are and consuming things...
+
+  */
+
   const { data, AlertFun, AlertFun2 } = useContext(AppContext);
   return (
     <div className="App">
